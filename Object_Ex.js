94ddd8bfae748ca4ab84a55dc95272faa3b99f5f
sushantsingh0062-sudo <sushantsingh0062@gmail.com>
2026-01-29 23:05:41 +0530

    first commit

---
+++ Object_Ex.js
@@ -0,0 +1,16 @@
+let book = {
+    title : "The Rich Dad Poor Dad",
+    author :"Robert Kiyosaki",
+    year :1990
+};
+console.log("author"+" "+book.title);
+console.log("author name:"+" "+book.author+"\n year "+" "+book.year);
+
+let mobile ={
+    brand :"nokiya",
+    price: 1600
+};
+console.log ("brand "+" " +mobile.brand);
+if (brand in mobile){
+    c
+}
